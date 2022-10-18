0970d3370db44c2a8637e5aebb9f7bcfccbe41a3
Janes Rodrigues <engaero.jns@gmail.com>
2022-10-18 09:54:23 -0300

    Criando front-end para futuramente conectar com api em spring

---
+++ front_spring/src/App.jsx
@@ -0,0 +1,12 @@
+import Formulario from "./Formulario";
+import Tabela from "./Tabela";
+import './App.css';
+
+export default ()=>{
+    return(
+        <>
+            <Formulario/>
+            <Tabela/>
+        </>
+    )
+}
---
+++ front_spring/src/Tabela.jsx
@@ -0,0 +1,22 @@
+export default()=>{
+    return(
+        <table className="table">
+            <thead> {/* Tag semantica de marca do cabeçalho da tabela */}
+                <tr> {/* Tag de linha */}
+                    <th>#</th> {/* Tag de identificação do cabeçalho */}
+                    <th>Nome</th>
+                    <th>Marca</th>
+                    <th>Selecionar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td> {/* Tag do corpo da tabela */}
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
